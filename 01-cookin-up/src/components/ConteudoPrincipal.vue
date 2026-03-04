e4203e3ffdcb235f66a7bd0e70f202847987e8d4
frankwsb <frankwsb@uninove.br>
2026-03-04 18:57:10 -0300

    Personalizando estilos

--- 01-cookin-up/src/components/ConteudoPrincipal.vue
+++ 01-cookin-up/src/components/ConteudoPrincipal.vue
@@ -23,7 +23,7 @@ export default {
             </span>
             <ul v-if="ingredientes.length" class="ingredientes-sua-lista">
                 <li v-for="ingrediente in ingredientes" v-bind:key="ingrediente">
-                  <Tag :texto="ingrediente" />
+                  <Tag :texto="ingrediente" :ativa="true" />
                 </li>
             </ul>
             <p v-else class="paragrafo lista-vazia">
