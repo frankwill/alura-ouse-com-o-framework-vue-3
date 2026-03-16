b9d464fd4918eb0d542f97532e8c1bb24c3ebdb4
frankwsb <frankwsb@uninove.br>
2026-03-16 18:28:52 -0300

    Vue-router e a primeira rota

--- 03-alura-tracker-avançado/src/App.vue
+++ 03-alura-tracker-avançado/src/App.vue
@@ -1,35 +1,18 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import BarraLateral from "./components/BarraLateral.vue";
-import Formulario from "./components/Formulario.vue";
-import Tarefa from "./components/Tarefa.vue";
-import type ITarefa from "./interface/ITarefa";
-import Box from "./components/Box.vue";
 
 export default defineComponent({
   name: "App",
   components: {
     BarraLateral,
-    Formulario,
-    Tarefa,
-    Box,
   },
   data() {
     return {
-      tarefas: [] as ITarefa[],
       modoEscuroAtivo: false,
     };
   },
-  computed: {
-    listaEstaVazia(): boolean {
-      return this.tarefas.length === 0;
-    },
-  },
   methods: {
-    salvarTarefa(tarefa: ITarefa) {
-      this.tarefas.push(tarefa);
-    },
-
     trocarTema(modoEscuroAtivo: boolean) {
       this.modoEscuroAtivo = modoEscuroAtivo;
     },
@@ -43,11 +26,7 @@ export default defineComponent({
       <BarraLateral @aoTemaAlterado="trocarTema" />
     </div>
     <div class="column is-three-quarter conteudo">
-      <Formulario @aoSalvarTarefa="salvarTarefa" />
-      <div class="lista">
-        <Tarefa v-for="(tarefa, index) in tarefas" :key="index" :tarefa="tarefa" />
-        <Box v-if="listaEstaVazia">Você não esta muito produtivo hoje 😢</Box>
-      </div>
+      <router-view></router-view>
     </div>
   </main>
 </template>
