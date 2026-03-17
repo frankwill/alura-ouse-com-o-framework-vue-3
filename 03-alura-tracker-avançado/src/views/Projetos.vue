a18fa42d52fcc899a617da6c4f934783da0a6208
frankwsb <frankwsb@uninove.br>
2026-03-17 19:31:16 -0300

    Cadastrando projetos

--- 03-alura-tracker-avançado/src/views/Projetos.vue
+++ 03-alura-tracker-avançado/src/views/Projetos.vue
@@ -1,12 +1,61 @@
 <template>
-  <h1>Projetos</h1>
+  <section class="projetos">
+    <h1 class="title">Projetos</h1>
+    <form @submit.prevent="salvar">
+      <div class="field">
+        <label for="nome-do-projeto" class="label">Nome do projeto</label>
+        <input type="text" class="input" v-model="nomeDoProjeto" id="nome-do-projeto" />
+      </div>
+      <div class="field">
+        <button class="button" type="submit">Salvar</button>
+      </div>
+    </form>
+    <table class="table is-fullwidth">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Nome</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="projeto in projetos" :key="projeto.id">
+          <td>{{ projeto.id }}</td>
+          <td>{{ projeto.nome }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </section>
 </template>
 
 <script lang="ts">
+import type IProjeto from "../interface/IProjeto";
 import { defineComponent } from "vue";
 
 export default defineComponent({
   // eslint-disable-next-line vue/multi-word-component-names
   name: "Projetos",
+  data() {
+    return {
+      nomeDoProjeto: "",
+      projetos: [] as IProjeto[],
+    };
+  },
+  methods: {
+    salvar() {
+      const projeto: IProjeto = {
+        nome: this.nomeDoProjeto,
+        id: new Date().toISOString(),
+      };
+
+      this.projetos.push(projeto);
+      this.nomeDoProjeto = "";
+    },
+  },
 });
 </script>
+
+<style scoped>
+.projetos {
+  padding: 1.25rem;
+}
+</style>
