01f56222cf5a2b39b209646d2245310096b467e7
frankwsb <frankwsb@uninove.br>
2026-03-19 19:22:06 -0300

    Editando um projeto

--- 03-alura-tracker-avançado/src/views/Projetos/Formulario.vue
+++ 03-alura-tracker-avançado/src/views/Projetos/Formulario.vue
@@ -20,6 +20,17 @@ import { defineComponent } from "vue";
 export default defineComponent({
   // eslint-disable-next-line vue/multi-word-component-names
   name: "Formulario",
+  props: {
+    id: {
+      type: String,
+    },
+  },
+  mounted() {
+    if (this.id) {
+      const projeto = this.store.state.projetos.find((proj) => proj.id === this.id);
+      this.nomeDoProjeto = projeto?.nome || "";
+    }
+  },
   data() {
     return {
       nomeDoProjeto: "",
@@ -27,7 +38,15 @@ export default defineComponent({
   },
   methods: {
     salvar() {
-      this.store.commit("ADICIONA_PROJETO", this.nomeDoProjeto);
+      if (this.id) {
+        this.store.commit("ALTERA_PROJETO", {
+          id: this.id,
+          nome: this.nomeDoProjeto,
+        });
+      } else {
+        this.store.commit("ADICIONA_PROJETO", this.nomeDoProjeto);
+      }
+
       this.nomeDoProjeto = "";
       this.$router.push("/projetos");
     },
