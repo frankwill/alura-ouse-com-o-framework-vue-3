8b871d41a0a3e0d1d3c0188776e37b63c873d666
frankwsb <frankwsb@uninove.br>
2026-03-19 18:58:10 -0300

    Setup da store

--- 03-alura-tracker-avançado/src/store/index.ts
+++ 03-alura-tracker-avançado/src/store/index.ts
@@ -1,6 +1,6 @@
 import type IProjeto from "../interface/IProjeto";
 import type { InjectionKey } from "vue";
-import { createStore, Store } from "vuex";
+import { createStore, Store, useStore as useStoreVuex } from "vuex";
 
 interface Estado {
   projetos: IProjeto[];
@@ -10,19 +10,19 @@ export const key: InjectionKey<Store<Estado>> = Symbol();
 
 export const store = createStore<Estado>({
   state: {
-    projetos: [
-      {
+    projetos: [],
+  },
+  mutations: {
+    ADICIONA_PROJETO(state, nomeDoProjeto: string) {
+      const projeto = {
         id: new Date().toISOString(),
-        nome: "TypeScript",
-      },
-      {
-        id: new Date().toISOString(),
-        nome: "Vue",
-      },
-      {
-        id: new Date().toISOString(),
-        nome: "Vuex",
-      },
-    ],
+        nome: nomeDoProjeto,
+      } as IProjeto;
+      state.projetos.push(projeto);
+    },
   },
 });
+
+export function useStore(): Store<Estado> {
+  return useStoreVuex(key);
+}
--- 03-alura-tracker-avançado/src/views/Projetos.vue
+++ 03-alura-tracker-avançado/src/views/Projetos.vue
@@ -28,8 +28,8 @@
 </template>
 
 <script lang="ts">
-import type IProjeto from "../interface/IProjeto";
-import { defineComponent } from "vue";
+import { useStore } from "@/store";
+import { computed, defineComponent } from "vue";
 
 export default defineComponent({
   // eslint-disable-next-line vue/multi-word-component-names
@@ -37,19 +37,20 @@ export default defineComponent({
   data() {
     return {
       nomeDoProjeto: "",
-      projetos: [] as IProjeto[],
     };
   },
   methods: {
     salvar() {
-      const projeto: IProjeto = {
-        nome: this.nomeDoProjeto,
-        id: new Date().toISOString(),
-      };
-
-      this.projetos.push(projeto);
+      this.store.commit("ADICIONA_PROJETO", this.nomeDoProjeto);
       this.nomeDoProjeto = "";
     },
+  },
+  setup() {
+    const store = useStore();
+    return {
+      store,
+      projetos: computed(() => store.state.projetos),
+    };
   },
 });
 </script>
