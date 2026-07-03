7c98862afb8f8320cc064284be1f76d6b824766a
frankwsb <frankwsb@uninove.br>
2026-07-03 16:20:46 -0300

    Refatorando o Tarefa.vue para composition api

--- 04-alura-tracker-composition-api-e-vuex/src/components/Tarefa.vue
+++ 04-alura-tracker-composition-api-e-vuex/src/components/Tarefa.vue
@@ -1,3 +1,17 @@
+<template>
+  <Box>
+    <div class="columns clicavel" @click="tarefaClicada">
+      <div class="column is-4">{{ tarefa.descricao || "Tarefa sem descrição" }}</div>
+      <div class="column is-3">
+        {{ tarefa.projeto.nome }}
+      </div>
+      <div class="column">
+        <Cronometro :tempoEmSegundos="tarefa.duracaoEmSegundos" />
+      </div>
+    </div>
+  </Box>
+</template>
+
 <script lang="ts">
 import { defineComponent, type PropType } from "vue";
 import Cronometro from "./Cronometro.vue";
@@ -17,27 +31,17 @@ export default defineComponent({
     },
   },
   emits: ["aoTarefaClicada"],
-  methods: {
-    tarefaClicada(): void {
-      this.$emit("aoTarefaClicada", this.tarefa);
-    },
+  setup(props, { emit }) {
+    const tarefaClicada = (): void => {
+      emit("aoTarefaClicada", props.tarefa);
+    };
+
+    return {
+      tarefaClicada,
+    };
   },
 });
 </script>
-
-<template>
-  <Box>
-    <div class="columns clicavel" @click="tarefaClicada">
-      <div class="column is-4">{{ tarefa.descricao || "Tarefa sem descrição" }}</div>
-      <div class="column is-3">
-        {{ tarefa.projeto.nome }}
-      </div>
-      <div class="column">
-        <Cronometro :tempoEmSegundos="tarefa.duracaoEmSegundos" />
-      </div>
-    </div>
-  </Box>
-</template>
 
 <style scoped>
 .clicavel {
