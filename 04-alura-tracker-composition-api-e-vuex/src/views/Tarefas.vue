18d847908dfc49d090d7bda43f0c711e9ffea8f1
frankwsb <frankwsb@uninove.br>
2026-07-03 16:42:30 -0300

    Filtrando tarefas

--- 04-alura-tracker-composition-api-e-vuex/src/views/Tarefas.vue
+++ 04-alura-tracker-composition-api-e-vuex/src/views/Tarefas.vue
@@ -1,5 +1,52 @@
+<template>
+  <Formulario @aoSalvarTarefa="salvarTarefa" />
+  <div class="lista">
+    <Box v-if="listaEstaVazia">Você não esta muito produtivo hoje 😢</Box>
+    <div class="field">
+      <p class="control has-icons-left">
+        <input v-model="filtro" class="input" type="text" placeholder="Digite para filtrar" />
+        <span class="icon is-small is-left">
+          <i class="fas fa-search"></i>
+        </span>
+      </p>
+    </div>
+    <Tarefa
+      v-for="(tarefa, index) in tarefas"
+      :key="index"
+      :tarefa="tarefa"
+      @aoTarefaClicada="selecionarTarefa"
+    />
+    <div class="modal" :class="{ 'is-active': tarefaSelecionada }" v-if="tarefaSelecionada">
+      <div class="modal-background"></div>
+      <div class="modal-card">
+        <header class="modal-card-head">
+          <p class="modal-card-title">Editando uma tarefa</p>
+          <button class="delete" aria-label="close" @click="fecharModal"></button>
+        </header>
+        <section class="modal-card-body">
+          <div class="field">
+            <label for="descricaoDaTarefa" class="label">Descrição</label>
+            <input
+              type="text"
+              class="input"
+              v-model="tarefaSelecionada.descricao"
+              id="descricaoDaTarefa"
+            />
+          </div>
+        </section>
+        <footer class="modal-card-foot">
+          <div class="buttons">
+            <button @click="alterarTarefa" class="button is-success">Salvar alterações</button>
+            <button @click="fecharModal" class="button">Cancelar</button>
+          </div>
+        </footer>
+      </div>
+    </div>
+  </div>
+</template>
+
 <script lang="ts">
-import { computed, defineComponent } from "vue";
+import { computed, defineComponent, ref } from "vue";
 import Formulario from "../components/Formulario.vue";
 import Tarefa from "../components/Tarefa.vue";
 import Box from "../components/Box.vue";
@@ -48,49 +95,17 @@ export default defineComponent({
     store.dispatch(OBTER_TAREFAS);
     store.dispatch(OBTER_PROJETOS);
 
+    const tarefas = computed(() =>
+      store.state.tarefas.filter((t) => !filtro.value || t.descricao.includes(filtro.value)),
+    );
+
+    const filtro = ref("");
+
     return {
-      tarefas: computed(() => store.state.tarefas),
+      filtro,
+      tarefas,
       store,
     };
   },
 });
 </script>
-
-<template>
-  <Formulario @aoSalvarTarefa="salvarTarefa" />
-  <div class="lista">
-    <Box v-if="listaEstaVazia">Você não esta muito produtivo hoje 😢</Box>
-    <Tarefa
-      v-for="(tarefa, index) in tarefas"
-      :key="index"
-      :tarefa="tarefa"
-      @aoTarefaClicada="selecionarTarefa"
-    />
-    <div class="modal" :class="{ 'is-active': tarefaSelecionada }" v-if="tarefaSelecionada">
-      <div class="modal-background"></div>
-      <div class="modal-card">
-        <header class="modal-card-head">
-          <p class="modal-card-title">Editando uma tarefa</p>
-          <button class="delete" aria-label="close" @click="fecharModal"></button>
-        </header>
-        <section class="modal-card-body">
-          <div class="field">
-            <label for="descricaoDaTarefa" class="label">Descrição</label>
-            <input
-              type="text"
-              class="input"
-              v-model="tarefaSelecionada.descricao"
-              id="descricaoDaTarefa"
-            />
-          </div>
-        </section>
-        <footer class="modal-card-foot">
-          <div class="buttons">
-            <button @click="alterarTarefa" class="button is-success">Salvar alterações</button>
-            <button @click="fecharModal" class="button">Cancelar</button>
-          </div>
-        </footer>
-      </div>
-    </div>
-  </div>
-</template>
